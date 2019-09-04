6141de156b7a6a698e03ee5ed84f78e4f7aff7f3
Alan Smillie <alan.smillie@gmail.com>
2019-09-04 14:34:59 -0400

    Add public folder and index

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Index.js')
-})
+const PUBLIC_PATH = path.join(__dirname, '../public/')
+
+app.use(express.static(PUBLIC_PATH))
 
 module.exports = app
