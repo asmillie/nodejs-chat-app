d2a843e097a2e4592c6317ed7ff4af386b14a9b1
Alan Smillie <alan.smillie@gmail.com>
2019-09-04 14:14:57 -0400

    Move app setup to separate file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-const express = require('express')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT | 3000
-
-app.get('/', (req, res) => {
-    res.send('Index.js')
-})
 
 app.listen(port, () => console.log(`Chat App powered by ExpressJs running on port ${port}`))
