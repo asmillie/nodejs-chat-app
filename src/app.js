d831309b0620d20654f04658a1cc8f44ccaf3580
Alan Smillie <alan.smillie@gmail.com>
2019-09-05 14:40:25 -0400

    Install socketio and initial setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const PUBLIC_PATH = path.join(__dirname, '../public/')
 
 app.use(express.static(PUBLIC_PATH))
 
-module.exports = app
+io.on('connection', () => {
+    console.log('New web socket connection')
+})
+
+module.exports = server
