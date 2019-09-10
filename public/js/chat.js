decaa481adadf17f1b9e21bc3384f790f9d1df50
Alan Smillie <alan.smillie@gmail.com>
2019-09-10 15:06:22 -0400

    Add location sharing

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,11 +5,23 @@ socket.on('message', (msg) => {
     msgContainer.insertAdjacentHTML('beforeend', `<p>${msg}</p>`)
 })
 
-const sendBtn = document.querySelector('#sendBtn')
-sendBtn.addEventListener('click', () => {
-    event.preventDefault()
-    const message = document.querySelector('#message')
+document.querySelector('#messageForm').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = e.target.elements.message
     socket.emit('sendMessage', message.value)
     message.value = ''
 })
 
+document.querySelector('#shareLocationBtn').addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by current browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
+})
--- src/app.js
+++ src/app.js
@@ -14,10 +14,19 @@ app.use(express.static(PUBLIC_PATH))
 io.on('connection', (socket) => {
     console.log('New web socket connection')
 
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome to chat app')
+    socket.broadcast.emit('message', 'User has joined')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (location) => {
+        io.emit('message', `Location: ${location.longitude}, ${location.latitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left')
     })
 })
 
