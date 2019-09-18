fe8bbba9c3a824b42019c7c2c03268f47fdcd746
Alan Smillie <alan.smillie@gmail.com>
2019-09-18 09:26:39 -0400

    Create user utils to manage users

---
+++ src/utils/users.utils.js
@@ -0,0 +1,56 @@
+const users = []
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter(user => user.room === room.toLowerCase())
+}
+
+const addUser = ({ id, name, room }) => {
+    // Validate
+    if (!name || !room) {
+        return {
+            error: 'Name and room are required'
+        }
+    }
+
+    // Sanitize
+    name = name.trim().toLowerCase()
+    room = room.trim().toLowerCase()    
+
+    const userExists = users.find((user) => {
+        return user.room === room && user.name === name
+    })
+
+    if (userExists) {
+        return {
+            error: 'Name already exists'
+        }
+    }
+
+    // Save user
+    const user = {
+        id, 
+        name,
+        room
+    }
+
+    users.push(user)
+    return user
+}
+
+const removeUser = (id) => {
+    const userIndex = user.findIndex((user) => user.id === id)
+    if (userIndex !== -1) {
+        return users.splice(userIndex, 1)[0]
+    }
+}
+
+module.exports = {
+    getUser,
+    getUsersInRoom,
+    addUser,
+    removeUser
+}
